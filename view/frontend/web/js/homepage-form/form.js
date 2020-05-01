8e57e2d75e84e3ef1aeee94def6773b3ecf12b57
Tymofii Maksymenko <tim.off.max@gmail.com>
2020-05-01 08:30:08 +0300

    Homepage Form. - UI fields settings corrected, form validation and reset implemented, common form styling.

--- view/frontend/web/js/homepage-form/form.js
+++ view/frontend/web/js/homepage-form/form.js
@@ -1,7 +1,40 @@
-define(['uiComponent'], function(Component) {
-    return Component.extend({
-        getTitle: function () {
-            return "Homepage Form";
-        }
+define([
+    'jquery',
+    'Magento_Ui/js/form/form',
+], function($, form) {
+    'use strict';
+
+    return form.extend({
+        initialize: function () {
+            this._super();
+
+            return this;
+        },
+
+        getTitle() {
+            return 'Homepage Form';
+        },
+
+        /**
+         * Validates each element and returns true, if all elements are valid.
+         */
+        validate: function () {
+            let isValid = true;
+
+            for (let field of this.elems()) {
+                let isNotInput = (typeof field.validate === 'undefined');
+                isValid = isNotInput || field.validate().valid;
+
+                if (!isValid && field.error()) {
+                    break;
+                }
+            }
+
+            if (false === isValid) {
+                this.focusInvalid();
+            }
+
+            return isValid;
+        },
     });
 });
